e12d1b0fce6f9a0ce758fb768eb5f7ef08fc1ace
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-17 00:07:14 -0500

    v0.0.1

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// handler function:
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Sime!!!"))
+}
+
+func main() {
+	// initializing mux as the server
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	// print a log message to say that the server is runnning
+	log.Print("Starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
