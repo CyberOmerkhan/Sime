cf3eea9cd2f42d71a9194b1e1f6f69b3f4fd9467
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-24 21:02:56 -0500

    v0.0.1

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,7 +10,7 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	ts, err := template.ParseFiles("../../ui/html/pages/home.tmpl")
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
