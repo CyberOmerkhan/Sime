0607a6c867d5e3002adf039a3710e74b8292c0f8
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-12-29 20:47:07 -0500

    v0.0.1

---
+++ fetch.js
@@ -0,0 +1,12 @@
+async function getData() {
+    try{
+        const url = 'https://dog.ceo/api/breeds/image/random'
+        const response = await fetch(url)
+        if(!response){
+            throw new Error(`Response Status: ${response.status}`)
+        }
+        console.log(response)
+    } catch(err) {
+        console.error(`Error: ${err.message}`)
+    }
+}
