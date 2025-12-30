763db687e330f01a5373a06b7867d1a221e657b8
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-12-29 21:15:57 -0500

    v0.0.1

--- fetch.js
+++ fetch.js
@@ -1,18 +1,10 @@
-async function getData() {
-    const url = `https://dog.ceo/api/breeds/imae/random`
-    try{
-        fetch(url).then((response) => {
-            if(!response.ok)
-                throw new Error(`Error code: ${response.status_code}`)
-            return response.json()
-        }).then((data) => { 
-            console.log(data)
-        })
-    } catch(err) {
-        console.error(`Error: ${err.message}`)
-    }
+const url = `https://apis.scrimba.com/bored/api/activity`
 
-    console.log("asdadsadasdsa")
-}
+fetch(url).then((response) => {
+    if(!response.ok)
+        throw new Error(`Error Code is ${response.status_code}`)
+    return response.json()
+}).then((data) => {
+    console.log(data)
+})
 
-getData()
