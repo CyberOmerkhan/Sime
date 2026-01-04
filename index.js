881a69c5b2ca8568e5d663b7c706b0aa93ea78e7
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-04 17:17:40 -0500

    v0.0.1

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ import dotenv from 'dotenv/config'
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
-    })
+})
 
-console.log(process.env.OPENAI_API_KEY)
+const response = await openai.responses.create({
+    model: "gpt-5-mini",
+    input: [
+        {
+            role: "user",
+            content: [
+                {
+                    type: "input_text",
+                    text: "What do you see in that image? Use web search and find me about this competition. Seemingly, this hackathon was in 2023. Find me who won in 2025.",
+                },
+                {
+                    type: "input_image",
+                    image_url: "https://nis.edu.kz/storage/news/321.jpg"
+                }
+            ]
+        }
+    ],
+    tools: [
+        {type: "web_search"}
+    ]
+})
+
+console.log(response.output_text)
+console.log(response.metadata)
+
+// console.log(process.env.OPENAI_API_KEY)
