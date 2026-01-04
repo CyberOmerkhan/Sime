5cc04b31c6da201b1f76adce8890b1be6ececa10
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-04 16:05:43 -0500

    v0.0.1

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import OpenAI from 'openai'
 import dotenv from 'dotenv/config'
 
-// const openai = new OpenAI({
-    // apiKey: process.env.OPENAI_API_KEY
-// })
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+    })
 
 console.log(process.env.OPENAI_API_KEY)
-// console.log(OpenAI)
