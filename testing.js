194ba38c282b3908e87984d4e97ff97c07abae02
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-05 17:57:52 -0500

    v0.0.1

--- testing.js
+++ testing.js
@@ -1,3 +1,20 @@
-import embedArr from './index.js'
+import arr from './index.js'
 
-console.log(embedArr)
+const n = arr.length
+
+const calculateCosineSimilarity = (arr1, arr2) => {
+    let product = 0.0, size1 = 0.0, size2 = 0.0
+    for(let i = 0; i < arr1.length; i++){
+        product += (arr1[i] * arr2[i])
+        size1 += arr1[i] * arr1[i]
+        size2 += arr2[i] * arr2[i] 
+    }
+    return product / (size1 * size2)
+}
+
+for(let i = 0; i < n; i++){
+    for(let j = i + 1; j < n; j++){
+        const score = calculateCosineSimilarity(arr[i], arr[j])
+        console.log(`Similarity score between word_${i} and word_${j} is: ${score}`)
+    }
+}
