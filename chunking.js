09318cbfd6e4a894cc5a370de8b3d18332b0f840
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-06 01:09:24 -0500

    v0.0.1

--- chunking.js
+++ chunking.js
@@ -1,21 +1,14 @@
-import {RecursiveCharacterTextSplitter} from "@langchain/textsplitters"
-import {readFile} from 'node:fs/promises'
+import {readFile} from 'fs/promises'
 
 async function fetchDocuments() {
     try{
         const data = await readFile('scrimba_info.txt', 'utf-8')
         return data
     } catch(err){
-        console.log(`Something went wrong: ${err}`)
+        throw new Error(`Something went wrong: ${err}`)
     }
 }
 
-const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-})
-
 const text = await fetchDocuments()
-
-console.log('adad')
-console.log(text, 'asd')
-console.log('adad')
+console.log(text)
+console.log(process.cwd())
