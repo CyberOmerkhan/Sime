f917da71212f3456201cb09ecfe33c771649eae7
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-10 12:09:29 -0500

    v0.0.1

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import http from 'node:http'
 import {serveStatic} from './utils/serveStatic.js'
+import { getData } from './utils/getData.js'
 
 const PORT = 8080
 const __dirname = import.meta.dirname
+const ghostlyData = await getData(__dirname)
+console.log(JSON.parse(ghostlyData))
 
 const server = http.createServer(async (req, res) => {
     serveStatic(res, req, __dirname)
---
+++ utils/getData.js
@@ -0,0 +1,14 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+export async function getData(dirname){
+    const dataDir = path.join(dirname, 'data')
+    const fileDir = path.join(dataDir, 'ghostlyData.json')
+    try {
+        const content = await fs.readFile(fileDir)
+        return content 
+    } catch(err){
+        console.log(`Error occured: ${err}`)
+        return ''
+    }
+}
