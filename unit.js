d302a02d0e5d07b548ae714abb4a60cb5f423fa1
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-12 18:58:17 -0500

    v0.0.1

---
+++ unit.js
@@ -0,0 +1,24 @@
+import { PGlite } from '@electric-sql/pglite';
+import fs from 'fs';
+
+(async () => {
+  const db = new PGlite();
+  const createTables = fs.readFileSync('create-tables.sql', 'utf8');
+  const insertCarsData = fs.readFileSync('insert-cars-data.sql', 'utf8');
+  await db.exec(createTables);
+  await db.exec(insertCarsData);
+  const crudOperations = fs.readFileSync('crud-operations.sql', 'utf8');
+  await db.exec(crudOperations);
+  const populateTables = fs.readFileSync('populate-tables.sql', 'utf8');
+  await db.exec(populateTables);
+  const alterTable = fs.readFileSync('alter-table.sql', 'utf-8');
+  await db.exec(alterTable);
+  const query = fs.readFileSync('query.sql', 'utf8');
+
+
+
+  const response = await db.query(query);
+
+  console.clear();
+  console.table(response.rows);
+})();
