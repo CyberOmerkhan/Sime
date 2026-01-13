269e86c59e9112c5ba63d05743be6d2720922420
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-13 10:16:03 -0500

    v0.0.1

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-import express from 'express'
+import express, { application } from 'express'
 import {startups} from './data/data.js'
 
 const app = express()
 
 app.get('/api', (req, res) => {
-    res.json(startups)
+    const queryObj = req.query
+    let filteredData = startups.filter((item) => {
+        return ((!queryObj.industry || item.industry.toLowerCase() === queryObj.industry.toLowerCase()) && 
+            (!queryObj.country || item.country.toLowerCase() === queryObj.country.toLowerCase()) && 
+            (!queryObj.continent || item.continent.toLowerCase() === queryObj.continent.toLowerCase()) && 
+            (!queryObj.is_seeking_funding || item.is_seeking_funding.toString() === queryObj.is_seeking_funding.toLowerCase()) && 
+            (!queryObj.has_mvp || item.has_mvp.toString() === queryObj.has_mvp.toLowerCase()))
+    })
+    res.json(filteredData)
 })
 
 app.listen(8000, () => console.log('listening 8000'))
