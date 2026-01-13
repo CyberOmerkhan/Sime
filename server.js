558b1e4a97eef2997fe34827a18eeaea2f5dfbe8
CyberOmerkhan <amazingOmerkhan@gmail.com>
2026-01-12 20:13:00 -0500

    v0.0.1

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('hell yeah')
+import express from 'express'
+
+const PORT = 8000;
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham"
+}
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(celebrity))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server connected to a port ${PORT}`)
+})
+
+
